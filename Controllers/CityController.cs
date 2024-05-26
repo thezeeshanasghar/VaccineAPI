c990a01dd247f303609581c457475047254b8ef7
alikhan00000 <106005013+alikhan00000@users.noreply.github.com>
2024-05-26 18:50:06 +0500

    ADDING updates api

--- Controllers/CityController.cs
+++ Controllers/CityController.cs
@@ -23,6 +23,12 @@ namespace VaccineAPI.Controllers
         {
             return await _context.Cities.ToListAsync();
         }
+        [HttpGet("Names")]
+        public async Task<ActionResult<IEnumerable<string>>> GetAllCityNames()
+        {
+            var cityNames = await _context.Cities.Select(city => city.Name).ToListAsync();
+            return cityNames;
+        }
 
         // GET: api/City/5
         [HttpGet("{id}")]
@@ -98,5 +104,95 @@ namespace VaccineAPI.Controllers
         {
             return _context.Cities.Any(e => e.Id == id);
         }
+        [HttpGet("CityAlert")]
+        public async Task<IEnumerable<string>> GetLatestPatientCitiesNotInCityTableAsync()
+        {
+            var latestCities = await _context.Childs
+            .OrderByDescending(p => p.Id)
+            .Take(3)
+            .Select(p => p.City)
+            .ToListAsync();
+
+            var existingCities = await _context.Cities
+                .Where(c => latestCities.Contains(c.Name))
+                .Select(c => c.Name)
+                .ToListAsync();
+
+            var citiesNotInCityTable = latestCities.Except(existingCities);
+            // if (citiesNotInCityTable.Any())
+            // {
+            //     var newCities = citiesNotInCityTable.Select(city => new City { Name = city }).ToList();
+            //     _context.Cities.AddRange(newCities);
+            //     await _context.SaveChangesAsync();
+            // }
+
+            return citiesNotInCityTable;
+        }
+        // [HttpPut("update")]
+        // public async Task<ActionResult<Response<Object>>> UpdateChildCity(string currentCity, [FromBody] string newCity)
+        // {
+        //     var AlreadyCity = await _context.Cities.FirstOrDefaultAsync(c => c.Name == newCity);
+        //     if (AlreadyCity != null)
+        //     {
+        //          return new ActionResult<Response<Object>>(new Response<Object>(false, "Cannot update the city because it already exists.", null));
+        //     }
+            
+        //     var childs = await _context.Childs.Where(c => c.City == currentCity).ToListAsync();
+        //     if (childs == null || !childs.Any())
+        //     {
+        //         return NotFound();
+        //     }
+
+        //     foreach (var child in childs)
+        //     {
+        //         // Update the city of each child with the new city
+        //         child.City = newCity;
+        //         _context.Childs.Update(child);
+        //     }
+            
+        //     if (AlreadyCity == null)
+        //     {
+        //         var city = new City { Name = newCity };
+        //         _context.Cities.Add(city);
+        //     }
+        
+        //     await _context.SaveChangesAsync();
+
+        //     return new Response<object>(true, "City updated successfully.", null);
+        // }
+        [HttpPut("update")]
+        public async Task<ActionResult<Response<Object>>> UpdateChildCity(string currentCity, [FromBody] string newCity)
+        {
+            var AlreadyCity = await _context.Cities.FirstOrDefaultAsync(c => c.Name == newCity);
+            if (AlreadyCity != null)
+            {
+                return BadRequest(new Response<Object>(false, "Cannot update the city because it already exists 1.", null));
+            }
+            
+            var childs = await _context.Childs.Where(c => c.City == currentCity).ToListAsync();
+            if (childs == null || !childs.Any())
+            {
+                return NotFound();
+            }
+
+            foreach (var child in childs)
+            {
+                // Update the city of each child with the new city
+                child.City = newCity;
+                _context.Childs.Update(child);
+            }
+            
+            if (AlreadyCity == null)
+            {
+                var city = new City { Name = newCity };
+                _context.Cities.Add(city);
+            }
+        
+            await _context.SaveChangesAsync();
+
+            return Ok(new Response<object>(true, "City updated successfully.", null));
+        }
+
+
     }
 }

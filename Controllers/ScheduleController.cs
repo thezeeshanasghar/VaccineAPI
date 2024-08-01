b7d34a27ab4aceea45ffd98c58e1de9f88557f63
Muhammad Abubakar <Ahohihi721@gmail.com>
2024-08-01 17:01:05 +0500

    Restore Changes in Alert Email

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -21,7 +21,6 @@ namespace VaccineAPI.Controllers
         private readonly IWebHostEnvironment _host;
 
         private readonly IMapper _mapper;
-        private long DoseId;
 
         public ScheduleController(Context context, IMapper mapper, IWebHostEnvironment host)
         {
@@ -1100,8 +1099,7 @@ namespace VaccineAPI.Controllers
                         ?.Doctor;
 
                     var clinics = _db.Clinics.Where(x => x.Id == ClinicId).FirstOrDefault();
-                    // Retrieve the dose information
-                    var doses = _db.Doses.Where(x => x.Id == DoseId).FirstOrDefault();
+
                     var dbSchedules = _db.Schedules.Where(x => x.ChildId == ChildId).ToList();
 
                     var specificDate = DateTime.Today;
@@ -1113,7 +1111,7 @@ namespace VaccineAPI.Controllers
                     if (specificSchedule != null)
                     {
                         var doseId = specificSchedule.DoseId;
-                        body = $"Reminder: Vaccination!! Doctor: {doctor.FirstName}, Clinic: {clinics.Name}, Phone: {clinics.PhoneNumber}, Child: {child.Name}, Dose ID: {doses.Name}";
+                        body = $"Reminder: Vaccination!! Doctor: {doctor.FirstName}, Clinic: {clinics.Name}, Phone: {clinics.PhoneNumber}, Child: {child.Name}, Dose ID: {doseId}";
                     }
                     else
                     {

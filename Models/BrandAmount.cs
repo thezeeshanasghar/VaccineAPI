deba2ad50fd489fab6f9f9ed70fd0761cb486cb3
Zeeshan Asghar <m.zeeshan.asghar@live.com>
2024-08-17 14:49:04 +0500

    remove files

--- Models/BrandAmount.cs
+++ Models/BrandAmount.cs
@@ -5,19 +5,19 @@ using Newtonsoft.Json;
 namespace VaccineAPI.Models
 {
 
-    public class BrandAmount
-    {
-        public long Id { get; set; }
-        public int Amount { get; set; }
-        public int Count { get; set; }
-        public long BrandId { get; set; }
-         [JsonIgnore]
-        public virtual Brand Brand { get; set; }
-    
-        public long DoctorId { get; set; }
-         [JsonIgnore]
-        public virtual Doctor Doctor { get; set; }
-      //  public virtual string VaccineName { get; set; }
-    }
+  public class BrandAmount
+  {
+    public long Id { get; set; }
+    public int Amount { get; set; }
+    public int Count { get; set; }
+    public long BrandId { get; set; }
+    [JsonIgnore]
+    public virtual Brand Brand { get; set; }
+
+    public long DoctorId { get; set; }
+    [JsonIgnore]
+    public virtual Doctor Doctor { get; set; }
+    //  public virtual string VaccineName { get; set; }
+  }
 
 }

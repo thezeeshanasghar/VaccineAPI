39b717cbe1ee7defa5a0b4b30bc2af9bb3ead374
Muhammad Atta Ur Rehman <73884338+themuhammadattaurrehman@users.noreply.github.com>
2024-11-22 22:04:50 +0500

    add dto

---
+++ ModelDTO/DashboardDTO.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System ;
+
+namespace VaccineAPI.ModelDTO
+{
+    public class DashboardDTO
+    {
+        public int CurrentMonthChildCount { get; set; }
+    }
+}

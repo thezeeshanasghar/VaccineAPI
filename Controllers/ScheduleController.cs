5079956c16a3bdfef7d7826c4680d4d1d2b829d6
Muhammad Atta Ur Rehman <73884338+themuhammadattaurrehman@users.noreply.github.com>
2024-12-23 16:35:10 +0500

    changes in schedule

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -995,26 +995,24 @@ namespace VaccineAPI.Controllers
         public async Task<Response<List<Schedule>>> Delete(long ChildId, long DoseId, String date)
         {
             DateTime dateOfInjection = DateTime.ParseExact(date, "dd-MM-yyyy", null);
-
+           
             var objList = await _db.Schedules
                 .Where(x => x.ChildId == ChildId)
                 .Where(x => x.DoseId == DoseId)
+                .Where(x => x.IsDone == false)
                 .Where(x => x.Date > dateOfInjection)
                 .ToListAsync();
-
-            List<Schedule> listDTO = _mapper.Map<List<Schedule>>(objList);
-
+            var futureDoses = objList.Where(x => x.Date > dateOfInjection).ToList();
+            List<Schedule> listDTO = _mapper.Map<List<Schedule>>(futureDoses);
             if (listDTO == null)
             {
                 return new Response<List<Schedule>>(false, "Error: failed to delete ", listDTO);
             }
-
             foreach (Schedule obj in listDTO)
             {
                 _db.Schedules.Remove(obj);
             }
             await _db.SaveChangesAsync();
-
             return new Response<List<Schedule>>(true, null, listDTO);
         }
 

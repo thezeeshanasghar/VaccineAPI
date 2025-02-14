b617c5f772d31490d16b0c91012d42f8da557248
Muhammad Abubakar <155424202+theabubakar@users.noreply.github.com>
2025-02-14 08:37:09 +0500

    Follow-Up PDF Created (#68)

--- Controllers/FollowUpController.cs
+++ Controllers/FollowUpController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VaccineAPI.Models;
 using AutoMapper;
 using VaccineAPI.ModelDTO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
 
 namespace VaccineAPI.Controllers
 {
@@ -145,6 +144,182 @@ namespace VaccineAPI.Controllers
             _db.FollowUps.Remove(obj);
             await _db.SaveChangesAsync();
             return Ok(new { Message = "Follow-up deleted successfully." });
+        }
+
+        [HttpGet("Follow-Up-PDF")]
+        public IActionResult GenerateFollowUpPdf(int childId)
+        {
+            var followUpDetails = _db.FollowUps
+                .Where(f => f.ChildId == childId)
+                .OrderBy(f => f.NextVisitDate)
+                .FirstOrDefault();
+
+            if (followUpDetails == null)
+            {
+                return NotFound("Follow-up details not found for the child");
+            }
+            DateTime nextVisitDate = followUpDetails.NextVisitDate ?? DateTime.MinValue;
+            string disease = followUpDetails.Disease;
+            float weight = followUpDetails.Weight ?? 0;
+            float height = followUpDetails.Height ?? 0;
+            float ofc = followUpDetails.OFC ?? 0;
+
+            var childDetails = _db.Childs
+                .Include(c => c.Clinic)
+                .ThenInclude(clinic => clinic.Doctor)
+                .FirstOrDefault(c => c.Id == childId);
+
+            if (childDetails == null)
+            {
+                return NotFound("Child not found");
+            }
+            string clinicName = childDetails.Clinic.Name;
+            string doctorDetails = $"Dr {childDetails.Clinic.Doctor.DisplayName}\n{childDetails.Clinic.Doctor.Qualification}";
+            string clinicAddress = childDetails.Clinic.Address;
+            string childName = childDetails.Name;
+            string fatherName = childDetails.FatherName;
+
+            var output = new MemoryStream();
+            var document = new Document();
+            PdfWriter.GetInstance(document, output);
+            document.Open();
+            var headerTable = new PdfPTable(2);
+            headerTable.WidthPercentage = 100;
+            headerTable.SetWidths(new float[] { 3, 1 });
+
+            PdfPCell headerCell = new PdfPCell();
+            headerCell.Border = PdfPCell.NO_BORDER;
+            headerCell.AddElement(new Paragraph(clinicName, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 12)));
+            headerCell.AddElement(new Paragraph(doctorDetails, FontFactory.GetFont(FontFactory.HELVETICA, 8)));
+            headerCell.AddElement(new Paragraph(clinicAddress, FontFactory.GetFont(FontFactory.HELVETICA, 8)));
+            headerTable.AddCell(headerCell);
+
+            string logoPath = Path.Combine(Directory.GetCurrentDirectory(), "Resources", "Images", "clinicLogo.png");
+            if (System.IO.File.Exists(logoPath))
+            {
+                var logo = Image.GetInstance(logoPath);
+                logo.ScaleToFit(150f, 150f);
+                PdfPCell logoCell = new PdfPCell(logo)
+                {
+                    Border = PdfPCell.NO_BORDER,
+                    HorizontalAlignment = Element.ALIGN_RIGHT
+                };
+                headerTable.AddCell(logoCell);
+            }
+            else
+            {
+                headerTable.AddCell(new PdfPCell(new Phrase("No Logo Available", FontFactory.GetFont(FontFactory.HELVETICA, 10))) { Border = PdfPCell.NO_BORDER });
+            }
+
+            document.Add(headerTable);
+            var childInfoHeading = new Paragraph("Child Info", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 12))
+            {
+                SpacingBefore = 2, 
+                Alignment = Element.ALIGN_CENTER 
+            };
+            document.Add(childInfoHeading);
+            var childDetailsTable = new PdfPTable(2);
+            childDetailsTable.WidthPercentage = 100;
+            childDetailsTable.SpacingBefore = 10;
+
+            childDetailsTable.AddCell(new PdfPCell(new Phrase("Child Name", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10)))
+            {
+                BackgroundColor = BaseColor.LightGray,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                Padding = 5 
+            });
+            childDetailsTable.AddCell(new PdfPCell(new Phrase(childName, FontFactory.GetFont(FontFactory.HELVETICA, 10)))
+            {
+                BackgroundColor = BaseColor.White,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                Padding = 5 
+            });
+
+            childDetailsTable.AddCell(new PdfPCell(new Phrase("Father Name", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10)))
+            {
+                BackgroundColor = BaseColor.LightGray,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                Padding = 5 
+            });
+            childDetailsTable.AddCell(new PdfPCell(new Phrase(fatherName, FontFactory.GetFont(FontFactory.HELVETICA, 10)))
+            {
+                BackgroundColor = BaseColor.White,
+                HorizontalAlignment = Element.ALIGN_LEFT,
+                Padding = 5 
+            });
+
+            document.Add(childDetailsTable);
+
+            var followUpHeaderTable = new PdfPTable(2);
+            followUpHeaderTable.WidthPercentage = 100;
+            followUpHeaderTable.SpacingBefore = 10;
+            var followUpDetailsHeading = new Paragraph("Next Follow Up Details", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10))
+            {
+                SpacingBefore = 20, 
+                Alignment = Element.ALIGN_CENTER
+            };
+            document.Add(followUpDetailsHeading);
+
+            followUpHeaderTable.AddCell(new PdfPCell(new Phrase("Next Visit", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 9)))
+            {
+                BackgroundColor = BaseColor.LightGray,
+                HorizontalAlignment = Element.ALIGN_LEFT
+            });
+            followUpHeaderTable.AddCell(new PdfPCell(new Phrase(nextVisitDate.ToString("dd-MM-yyyy"), FontFactory.GetFont(FontFactory.HELVETICA, 10)))
+            {
+                BackgroundColor = BaseColor.White,
+                HorizontalAlignment = Element.ALIGN_LEFT
+            });
+
+            followUpHeaderTable.AddCell(new PdfPCell(new Phrase("Disease", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10)))
+            {
+                BackgroundColor = BaseColor.LightGray,
+                HorizontalAlignment = Element.ALIGN_LEFT
+            });
+            followUpHeaderTable.AddCell(new PdfPCell(new Phrase(disease, FontFactory.GetFont(FontFactory.HELVETICA, 10)))
+            {
+                BackgroundColor = BaseColor.White,
+                HorizontalAlignment = Element.ALIGN_LEFT
+            });
+
+            followUpHeaderTable.AddCell(new PdfPCell(new Phrase("Weight", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10)))
+            {
+                BackgroundColor = BaseColor.LightGray,
+                HorizontalAlignment = Element.ALIGN_LEFT
+            });
+            followUpHeaderTable.AddCell(new PdfPCell(new Phrase($"{weight} kg", FontFactory.GetFont(FontFactory.HELVETICA, 10)))
+            {
+                BackgroundColor = BaseColor.White,
+                HorizontalAlignment = Element.ALIGN_LEFT
+            });
+
+            followUpHeaderTable.AddCell(new PdfPCell(new Phrase("Height", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10)))
+            {
+                BackgroundColor = BaseColor.LightGray,
+                HorizontalAlignment = Element.ALIGN_LEFT
+            });
+            followUpHeaderTable.AddCell(new PdfPCell(new Phrase($"{height} cm", FontFactory.GetFont(FontFactory.HELVETICA, 10)))
+            {
+                BackgroundColor = BaseColor.White,
+                HorizontalAlignment = Element.ALIGN_LEFT
+            });
+
+            followUpHeaderTable.AddCell(new PdfPCell(new Phrase("OFC", FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 10)))
+            {
+                BackgroundColor = BaseColor.LightGray,
+                HorizontalAlignment = Element.ALIGN_LEFT
+            });
+            followUpHeaderTable.AddCell(new PdfPCell(new Phrase($"{ofc} cm", FontFactory.GetFont(FontFactory.HELVETICA, 10)))
+            {
+                BackgroundColor = BaseColor.White,
+                HorizontalAlignment = Element.ALIGN_LEFT
+            });
+
+            document.Add(followUpHeaderTable);
+
+            document.Close();
+            output.Seek(0, SeekOrigin.Begin);
+            return File(output, "application/pdf", "Follow-Up.pdf");
         }
     }
 }

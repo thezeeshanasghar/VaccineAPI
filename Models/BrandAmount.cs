ef437160da7b614559825b57c0971fd192e8dc54
Muhammad Atta Ur Rehman <73884338+themuhammadattaurrehman@users.noreply.github.com>
2025-02-26 22:46:37 +0500

    fix api fields (#78)
    * fix api fields
    
    * putt api fixed

--- Models/BrandAmount.cs
+++ Models/BrandAmount.cs
@@ -4,20 +4,22 @@ using Newtonsoft.Json;
 
 namespace VaccineAPI.Models
 {
+    public class BrandAmount
+    {
+        public long Id { get; set; }
+        public int Amount { get; set; }
+        public int Count { get; set; }
 
-  public class BrandAmount
-  {
-    public long Id { get; set; }
-    public int Amount { get; set; }
-    public int Count { get; set; }
-    public long BrandId { get; set; }
-    [JsonIgnore]
-    public virtual Brand Brand { get; set; }
+        public string SupName { get; set; } // Supplier Name
+        public int PurchasedAmt { get; set; } // Purchased Vaccine Amount
+        public bool IsPaid { get; set; } // Payment Status
 
-    public long DoctorId { get; set; }
-    [JsonIgnore]
-    public virtual Doctor Doctor { get; set; }
-    //  public virtual string VaccineName { get; set; }
-  }
+        public long BrandId { get; set; }
+        [JsonIgnore]
+        public virtual Brand Brand { get; set; }
 
+        public long DoctorId { get; set; }
+        [JsonIgnore]
+        public virtual Doctor Doctor { get; set; }
+    }
 }

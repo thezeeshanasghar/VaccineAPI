bfa405db24f8c676c7c8004f9f3c2dc668728470
themuhammadattaurrehman <atta.randhawa86@gmail.com>
2025-06-13 16:32:23 +0500

    add CSV export functionality for birthdays and follow-ups; create BirthdayCsvDTO and update FollowUpCsvDTO

--- ModelDTO/BirthdayCsvDTO.cs
+++ ModelDTO/BirthdayCsvDTO.cs
@@ -2,22 +2,17 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System;
-
 namespace VaccineAPI.ModelDTO
 {
-public class FollowUpCsvDTO
+public class BirthdayCsvDTO
 {
     public string ChildName { get; set; }
     public string FatherName { get; set; }
     public string DOB { get; set; }
     public string ClinicName { get; set; }
-    public string CurrentVisitDate { get; set; }
-    public string NextVisitDate { get; set; }
-    public string Disease { get; set; }
-    public string Weight { get; set; }
-    public string Height { get; set; }
-    public string OFC { get; set; }
-    public string Email { get; set; }
+    public string DoctorName { get; set; }
+    public int Age { get; set; }
     public string Phone { get; set; }
+    public string Email { get; set; }
 }
 }

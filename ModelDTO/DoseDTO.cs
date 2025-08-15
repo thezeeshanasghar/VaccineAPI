ef3a30328aded845db86f6d6d37055b2fbd3a4ff
Muhammad Atta Ur Rehman <73884338+themuhammadattaurrehman@users.noreply.github.com>
2025-08-15 16:48:01 +0500

    changes implemented as required (#186)

--- ModelDTO/DoseDTO.cs
+++ ModelDTO/DoseDTO.cs
@@ -14,6 +14,8 @@ namespace VaccineAPI.ModelDTO
         public int? MinGap { get; set; }
         public int? DoseOrder { get; set; }
         public long VaccineId { get; set; }
+        public string CountryCode { get; set; }
+        public string PhoneNumber { get; set; }
         public ClinicDTO Clinic { get; set; }
         public VaccineDTO Vaccine { get; set; }
     }

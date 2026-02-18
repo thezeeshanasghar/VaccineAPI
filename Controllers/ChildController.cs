7d8a30d194fb27c1e3fd65e65bccccd469317e0c
thezeeshanasghar <m.zeeshan.asghar@live.com>
2026-02-18 16:12:06 +0500

    refactor footer layout in PDF generation of ChildController by adjusting clinic header positioning for improved presentation

--- Controllers/ChildController.cs
+++ Controllers/ChildController.cs
@@ -4150,8 +4150,8 @@ int rowCount = 0;
         Phrase phrase = new Phrase();
         phrase.Add(new Chunk($"{clinicName} ", footerFont1));
         phrase.Add(new Chunk($"({regNo})", footerFont));
-        float clinicLineY = footerY + -13;
-        ColumnText.ShowTextAligned(cb, Element.ALIGN_LEFT, phrase, document.LeftMargin + 5, clinicLineY, 0);
+        float clinicHeaderY = document.BottomMargin + 34f;
+        ColumnText.ShowTextAligned(cb, Element.ALIGN_LEFT, phrase, document.LeftMargin + 5, clinicHeaderY, 0);
 
         // 3-column table for address, phone, email
         PdfPTable contactTable = new PdfPTable(3);
